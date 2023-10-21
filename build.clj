f049afd7b691c8edc7e846124a0b4f2e6318d2cd
Adrian Smith <smith.adriane@gmail.com>
2023-10-21 15:37:58 -0700

    change version to 1.0-SNAPSHOT

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/clogif)
-(def version "1.0")
+(def version "1.0-SNAPSHOT")
 
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
